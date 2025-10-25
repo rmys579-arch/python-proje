fad6a7b8cde9c4240eb9e9475491e0fe9a842b9f
Ceyda111 <kirmizibayrakceyda@gmail.com>
2025-10-25 14:35:37 +0300

    Update dosya_yonetimi.py

--- dosya_yonetimi.py
+++ dosya_yonetimi.py
@@ -2,7 +2,7 @@
 import os # dosyayı silmemiz yeniden adlandıramız veya sınıflandırmamız için 
 import shutil # dosyaları taşımak için 
 # sınıflandırma (sözlük) partı
-Sınıflandırma = {   
+tipler = {   
     "Belge": [ ".pdf" , ".docx" , ".txt" , ],   
     "Görsel": [ ".jpg", ".jpeg", ".gif"   ],
     "Sunum" : [ ".pptx" , ".ppt", ".key",  ]
@@ -11,3 +11,11 @@ Sınıflandırma = {
 
 #Dosyanın tipini bulalım
 def dosyanın_tipi (uzantı):
+ "" # dosyanın ismini küçük harfe çevir !!!!!!! .pdf ve .PDF in  aynı tipte olması için yapıyoruz
+ uzantı = uzantı.lower() 
+ for tip_adı , uzantılar in uzantı:
+  if uzantı in uzantılar:
+   return tip_adı
+  else:
+   return "diğer"
+  
